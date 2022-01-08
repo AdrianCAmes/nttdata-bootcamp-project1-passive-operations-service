78a96bb38a2180cb3ee3aef782405b7e14c4085a
Adrian Crespo Ames <Adrian_Ames@outlook.com>
2022-01-08 01:01:10 -0500

    Initial project architecture

---
+++ src/main/java/com/nttdata/bootcamp/passiveoperationsservice/PassiveOperationsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.passiveoperationsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassiveOperationsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassiveOperationsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/bootcamp/passiveoperationsservice/PassiveOperationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.passiveoperationsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassiveOperationsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
