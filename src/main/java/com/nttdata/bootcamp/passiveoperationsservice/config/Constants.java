fde1f93cd548c3e963328fd59da7e854b0f22c27
Adrian Crespo Ames <Adrian_Ames@outlook.com>
2022-01-22 07:01:54 -0500

    Pipeline update for deployment

--- src/main/java/com/nttdata/bootcamp/passiveoperationsservice/config/Constants.java
+++ src/main/java/com/nttdata/bootcamp/passiveoperationsservice/config/Constants.java
@@ -7,84 +7,84 @@ import org.springframework.stereotype.Component;
 @Component
 @Getter
 public class Constants {
-    @Value("${constants.eureka.service-url.customer-info-service:}")
+    @Value("${constants.eureka.service-url.customer-info-service}")
     private String customerInfoServiceUrl;
 
-    @Value("${constants.eureka.service-url.active-operations-service:}")
+    @Value("${constants.eureka.service-url.active-operations-service}")
     private String activeOperationsServiceUrl;
 
-    @Value("${constants.eureka.service-url.gateway-service:}")
+    @Value("${constants.eureka.service-url.gateway-service}")
     private String gatewayServiceUrl;
 
-    @Value("${constants.customer.personal-group.name:}")
+    @Value("${constants.customer.personal-group.name}")
     private String customerPersonalGroup;
 
-    @Value("${constants.customer.personal-group.subgroup.standard:}")
+    @Value("${constants.customer.personal-group.subgroup.standard}")
     private String customerPersonalStandardSubgroup;
 
-    @Value("${constants.customer.personal-group.subgroup.vip:}")
+    @Value("${constants.customer.personal-group.subgroup.vip}")
     private String customerPersonalVipSubgroup;
 
-    @Value("${constants.customer.business-group.name:}")
+    @Value("${constants.customer.business-group.name}")
     private String customerBusinessGroup;
 
-    @Value("${constants.customer.business-group.subgroup.standard:}")
+    @Value("${constants.customer.business-group.subgroup.standard}")
     private String customerBusinessStandardSubgroup;
 
-    @Value("${constants.customer.business-group.subgroup.pyme:}")
+    @Value("${constants.customer.business-group.subgroup.pyme}")
     private String customerBusinessPymeSubgroup;
 
-    @Value("${constants.account.current-group.name:}")
+    @Value("${constants.account.current-group.name}")
     private String accountCurrentGroup;
 
-    @Value("${constants.account.current-group.subgroup.standard:}")
+    @Value("${constants.account.current-group.subgroup.standard}")
     private String accountCurrentStandardSubgroup;
 
-    @Value("${constants.account.current-group.subgroup.pyme:}")
+    @Value("${constants.account.current-group.subgroup.pyme}")
     private String accountCurrentPymeSubgroup;
 
-    @Value("${constants.account.savings-group.name:}")
+    @Value("${constants.account.savings-group.name}")
     private String accountSavingsGroup;
 
-    @Value("${constants.account.savings-group.subgroup.standard:}")
+    @Value("${constants.account.savings-group.subgroup.standard}")
     private String accountSavingsStandardSubgroup;
 
-    @Value("${constants.account.savings-group.subgroup.vip:}")
+    @Value("${constants.account.savings-group.subgroup.vip}")
     private String accountSavingsVipSubgroup;
 
-    @Value("${constants.account.long-term-group.name:}")
+    @Value("${constants.account.long-term-group.name}")
     private String accountLongTermGroup;
 
-    @Value("${constants.account.long-term-group.subgroup.standard:}")
+    @Value("${constants.account.long-term-group.subgroup.standard}")
     private String accountLongTermStandardSubgroup;
 
-    @Value("${constants.status.blocked:}")
+    @Value("${constants.status.blocked}")
     private String statusBlocked;
 
-    @Value("${constants.status.active:}")
+    @Value("${constants.status.active}")
     private String statusActive;
 
-    @Value("${constants.operation.deposit-type:}")
+    @Value("${constants.operation.deposit-type}")
     private String operationDepositType;
 
-    @Value("${constants.operation.withdrawal-type:}")
+    @Value("${constants.operation.withdrawal-type}")
     private String operationWithdrawalType;
 
-    @Value("${constants.operation.transfer-out-type:}")
+    @Value("${constants.operation.transfer-out-type}")
     private String operationTransferOutType;
 
-    @Value("${constants.operation.transfer-in-type:}")
+    @Value("${constants.operation.transfer-in-type}")
     private String operationTransferInType;
 
-    @Value("${constants.circuit-breaker.customer-info-service.name:customersDefaultCircuitBreaker}")
+    @Value("${constants.circuit-breaker.customer-info-service.name}")
     private String customersServiceCircuitBreakerName;
 
-    @Value("${constants.circuit-breaker.customer-info-service.timeout:2}")
+    @Value("${constants.circuit-breaker.customer-info-service.timeout}")
     private Integer customersServiceCircuitBreakerTimeout;
 
-    @Value("${constants.circuit-breaker.active-operations-service.name:activesDefaultCircuitBreaker}")
+    @Value("${constants.circuit-breaker.active-operations-service.name}")
     private String activesServiceCircuitBreakerName;
 
-    @Value("${constants.circuit-breaker.active-operations-service.timeout:2}")
+    @Value("${constants.circuit-breaker.active-operations-service.timeout}")
     private Integer activesServiceCircuitBreakerTimeout;
 }
